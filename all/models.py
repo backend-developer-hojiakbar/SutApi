012b4a4001e11543285878be27e47161bd871674
backend-developer-hojiakbar <soyibnazarovhoji@gmail.com>
2025-03-19 10:04:58 +0500

    new

--- all/models.py
+++ all/models.py
@@ -350,7 +350,7 @@ from django.db import transaction
 
 class SotuvQaytarish(models.Model):
     id = models.BigAutoField(primary_key=True, editable=False)
-    sana = models.DateTimeField(auto_now_add=True)
+    sana = models.DateField(auto_now_add=True)
     qaytaruvchi = models.ForeignKey('all.User', on_delete=models.CASCADE, related_name='qaytarishlar')
     total_sum = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     ombor = models.ForeignKey('Ombor', on_delete=models.CASCADE)
